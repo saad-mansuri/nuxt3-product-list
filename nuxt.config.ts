cbfe59a0c94afd814e9e47cb224c76b2029e1dd9
saad-mansuri <saadmansuri249@gmail.com>
2025-12-25 04:02:29 +0530

    scss installed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
+   css: ['~/assets/css/tailwind.scss'],
   runtimeConfig: {
     public: {
       apiUrl: process.env.API_URL
